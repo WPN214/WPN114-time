ab4556484f75dbaa14ba1461ecf02e28964e9fb3
Pierre Cochard <pchd@quarre.fr>
2019-08-20 17:36:19 +0200

    loopable timenodes

--- source/time.hpp
+++ source/time.hpp
@@ -15,7 +15,10 @@ class TimeNode : public Node
     WPN_DECLARE_AUDIO_INPUT             (speed, 1)
     WPN_DECLARE_AUDIO_INPUT             (wait, 1)
     WPN_DECLARE_AUDIO_INPUT             (cancel, 1)
+    WPN_DECLARE_AUDIO_INPUT             (loop, 1)
     WPN_DECLARE_DEFAULT_AUDIO_OUTPUT    (end_out, 1)
+    WPN_DECLARE_AUDIO_OUTPUT            (phase_out, 1)
+    WPN_DECLARE_AUDIO_OUTPUT            (lap_out, 1)
 
 protected:
 
@@ -23,18 +26,24 @@ protected:
     m_phase = 0,
     m_rate = 0;
 
-    enum inputs     { begin, end_in, duration, speed, wait, cancel };
-    enum outputs    { end_out };
+    enum inputs     { begin, end_in, duration, speed, wait, cancel, loop, num_inputs };
+    enum outputs    { end_out, phase_out, lap_out, num_outputs };
     enum state      { off, waiting, running, ended };
 
     state
     m_state = off;
+
+    int
+    m_laps = 0;
 
 public:
 
     //---------------------------------------------------------------------------------------------
     Q_SIGNAL void
     start();
+
+    Q_SIGNAL void
+    lap(int lap);
 
     Q_SIGNAL void
     end();
@@ -81,12 +90,16 @@ public:
         auto wait       = inputs.audio[inputs::wait][0];
         auto duration   = inputs.audio[inputs::duration][0];
         auto cancel     = inputs.audio[inputs::cancel][0];
+        auto loop       = inputs.audio[inputs::loop][0];
         auto end_out    = outputs.audio[outputs::end_out][0];
+        auto phase_out  = outputs.audio[outputs::phase_out][0];
+        auto lap_out    = outputs.audio[outputs::lap_out][0];
 
         // speed TODO
         auto phase  = m_phase;
         auto rate   = m_rate;
         auto state  = m_state;
+        auto laps   = m_laps;
 
         for (vector_t f = 0; f < nframes; ++f)
         {
@@ -107,14 +120,13 @@ public:
             case state::running:
             {
                 if (phase >= duration[f]) {
-                    state = ended;
-                    QMetaObject::invokeMethod(this, "end", Qt::QueuedConnection);
-                    end_out[f] = 1;
-                    phase = 0;
+                    // report below
+                    end_in[f] = 1;
                 } else if (duration[f] >= 0) {
                     // if duration is infinite, do not increment phase?
                     // to be tested
                     phase++;
+                    phase_out[f] = phase/duration[f];
                 }
 
                 break;
@@ -122,17 +134,36 @@ public:
             }
 
             if (cancel[f] > 0) {
-                // cancel will always have the highest priority
+                // cancel signal will always have the highest priority
                 // we don't output or signal anything
+                // just a silent reset
                 phase = 0;
                 state = off;
             }
 
-            else if (end_in[f] > 0) {
-                // in that case, we output the end signals
-                end_out[f] = 1;
-                QMetaObject::invokeMethod(this, "end", Qt::QueuedConnection);
-                state = ended;
+            else if (end_in[f] > 0)
+            {
+                // if end signal
+                // we first check for loops
+
+                if (loop[f] != 0 && laps < loop[f])
+                {
+                    lap_out[f] = laps++;
+                    QMetaObject::invokeMethod(this, "lap", Qt::QueuedConnection, Q_ARG(int, laps));
+                    m_laps = laps;
+                    if (wait[f] > 0)
+                         state = waiting;
+                    else state = running;
+                }
+                else
+                {
+                    // final ending
+                    end_out[f]   = 1;
+                    QMetaObject::invokeMethod(this, "end", Qt::QueuedConnection);
+                    state = ended;
+                }
+
+                phase_out[f] = 1;
                 phase = 0;
             }
 
@@ -186,35 +217,16 @@ public:
     rwrite(pool& inputs, pool& outputs, vector_t nframes) override
     //---------------------------------------------------------------------------------------------
     {
+        TimeNode::rwrite(inputs, outputs, nframes);
 
+        auto from   = inputs.audio[num_inputs+1][0];
+        auto to     = inputs.audio[num_inputs+2][0];
+        auto phase  = outputs.audio[phase_out][0];
+        auto out    = outputs.audio[num_outputs+1][0];
 
-    }
-};
-
-//=================================================================================================
-class Loop : public TimeNode
-//=================================================================================================
-{
-    Q_OBJECT
-
-    WPN_DECLARE_AUDIO_INPUT(times, 1)
-    WPN_DECLARE_AUDIO_OUTPUT(lap, 1)
-
-public:
-
-    //---------------------------------------------------------------------------------------------
-    Loop()
-    //---------------------------------------------------------------------------------------------
-    {
-
-    }
-
-    //---------------------------------------------------------------------------------------------
-    virtual void
-    rwrite(pool& inputs, pool& outputs, vector_t nframes) override
-    //---------------------------------------------------------------------------------------------
-    {
-
+        for (vector_t f = 0; f < nframes; ++f)
+            // for now, just a linear interpolation
+            out[f] = lininterp(phase[f], from[f], to[f]);
     }
 };
 
